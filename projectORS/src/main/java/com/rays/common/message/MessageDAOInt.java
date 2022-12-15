96825905b77a8a6eff6da1e11adb603ea6df1aa5
bijendra08 <bijendra08@gmail.com>
2022-12-15 16:57:05 +0530

    success

---
+++ projectORS/src/main/java/com/rays/common/message/MessageDAOInt.java
@@ -0,0 +1,12 @@
+package com.rays.common.message;
+
+import com.rays.common.BaseDAOInt;
+
+/**
+ * MessageDAO interface.
+ * @author Bijendra Singh
+ * 
+ */
+public interface MessageDAOInt extends BaseDAOInt<MessageDTO> {
+
+}
